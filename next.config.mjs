49d80e6c928fa0627baab5c97aae597ada5ac4a0
UNNATI SINGH <146465718+UnnatiSingh1@users.noreply.github.com>
2025-07-24 01:14:53 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['placeholder.svg'],
+    unoptimized: true,
+  },
+}
+
+export default nextConfig;
+
